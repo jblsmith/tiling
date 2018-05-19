3534f7cb791cc1f7515aadd9ec515b123e84236f
Jordan Smith <jblsmith@gmail.com>
2018-05-19 21:15:43 +0200

    Turned tiles into a class handling main tasks (writing tiles, naming them, defining edge colours)

--- tiles.py
+++ tiles.py
@@ -189,10 +189,111 @@ for bg_color_name in ['b','w']:
 		filename = bg_color_name + "_mini-" + label
 		write_elementtree(doc, filename)
 
+
+# In order to handle a larger list of tile types, we create an Tile class to generate tiles and handle the logic of their edges.
+import webcolors
+from xml.etree import ElementTree as et
+import numpy as np
+
+class Tile(object):
+	"""A tile, defined by a set of ellipse parameters and a background color.
 	
+	Attributes:
+		background_color: Any web color name, default is "white".
+		foreground_color: Any web color name, default is "black".
+		ellipses: A list of ellipse parameters, each a tuple (cx,cy,rx,ry), with:
+			cx, cy: centre of the ellipse where the centre with respect to a square tile centred at (0,0) with width = 2;
+			rx, ry: horizontal and vertial 'radii' of the ellipse in the same coordinate system.
+		name: A codename for the tile. E.g., 'b_ne' for a black-background tile with an ellipse in the NE corner.
+		tile_size: A tuple (width,height) giving the dimensions of the tile.
+		edge_color_names: A tuple of four colours giving the volour of the (top, right, bottom, left) edges of the tile.
+		edge_color_rgbs: Same as above, but with each colour given as an RGB tuple.
+	"""
+	
+	def __init__(self, bg_color="white", fg_color="black", ellipses=[], name="tmp", tile_size=(100,100)):
+		self.local_save_dir = "/Users/jordan/Documents/repositories/tiling/tiles"
+		self.bg_color = webcolors.name_to_rgb(bg_color)
+		self.fg_color = webcolors.name_to_rgb(fg_color)
+		self.ellipses = ellipses
+		self.name = name
+		self.tile_size = tile_size
+			
+	# def add_ellipses(self, ellipses):
+	# 	self.ellipses += ellipses
+	#
+	# def set_ellipses(self, ellipses):
+	# 	self.ellipses = ellipses
+	
+	def make_tile_path(self):
+		return self.local_save_dir + "/" + self.name + ".svg"
+	
+	def get_canvas_coords(self, single_ellipse_coords):
+		cx, cy, rx, ry = single_ellipse_coords
+		new_cx = (cx+1)/2.0*self.tile_size[0]
+		new_cy = (1-cy)/(2.0)*self.tile_size[1]
+		new_rx = rx/2.0*self.tile_size[0]
+		new_ry = ry/2.0*self.tile_size[1]
+		return [new_cx, new_cy, new_rx, new_ry]
+	
+	def get_ellipse_edge_colors(self):
+		# For each edge, check a few points along the edges and verify that all of them or none of them are in any ellipses.
+		point_range = np.linspace(-1,1,3)
+		top_points = [(x,1) for x in point_range]
+		rig_points = [(1,x) for x in point_range]
+		bot_points = [(x,-1) for x in point_range]
+		lef_points = [(-1,x) for x in point_range]
+		colors = [self.get_set_color(point_set,self.ellipses) for point_set in [top_points, rig_points, bot_points, lef_points]]
+		return colors
+	
+	def check_if_point_in_ellipse(self, point, ellipse):
+		# A point (x,y) is in an ellipse if (x-cx)^2/a^2 + (y-cy)^2/b^2 < 1,
+		# where the ellipse has width a, height b, and its centre is located at (cx,cy).
+		x,y = point
+		cx,cy,a,b = ellipse
+		return (np.power(x-cx,2)/np.power(a,2) + np.power(y-cy,2)/np.power(b,2))
+	
+	def get_set_color(self, points, ellipses):
+		point_tests = np.array([[self.check_if_point_in_ellipse(point, ellipse) for ellipse in self.ellipses] for point in points])
+		# Is every point in (or on the edge of) at least one ellipse?
+		test_inclusion = np.all([np.min(test_result) <= 1 for test_result in point_tests])
+		# Is every point outside (but possibly touching) ALL of the ellipses?
+		test_exclusion = np.all([np.min(test_result) >= 1 for test_result in point_tests])
+		if (test_inclusion and test_exclusion):
+			print "Cannot decide on colour for one edge, since all points in test set are tangent to ellipses. Assigning background colour."
+			return self.bg_color
+		elif test_inclusion:
+			return self.fg_color
+		elif test_exclusion:
+			return self.bg_color
+		elif (not test_inclusion and not test_exclusion):
+			print "Cannot decide on colour for one edge, since it fails exclusion and inclusion tests. Assigning background colour."
+			return self.bg_color
+	
+	def imagine_tile_layout(self):
+		# Create blank canvas of appropriate size:
+		self.doc = et.Element('svg', width=str(self.tile_size[0]), height=str(self.tile_size[1]), version='1.1', xmlns='http://www.w3.org/2000/svg')
+		# Paint in the background rectangle:
+		et.SubElement(self.doc, 'rect', width=str(self.tile_size[0]), height=str(self.tile_size[1]), fill='rgb({0}, {1}, {2})'.format(self.bg_color[0],self.bg_color[1],self.bg_color[2]))
+		# Paint in the foreground circles:
+		for single_ellipse_coords in self.ellipses:
+			[cx, cy, rx, ry] = self.get_canvas_coords(single_ellipse_coords)
+			et.SubElement(self.doc, 'ellipse', cx=str(cx), cy=str(cy), rx=str(rx), ry=str(ry), fill='rgb({0}, {1}, {2})'.format(self.fg_color[0],self.fg_color[1],self.fg_color[2]))
+	
+	def write_tile_layout(self):
+		f = open(self.make_tile_path(), 'w')
+		f.write('<?xml version=\"1.0\" standalone=\"no\"?>\n')
+		f.write('<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n')
+		f.write('\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n')
+		f.write(et.tostring(self.doc))
+		f.close()
+	
+	def create_tile_image(self):
+		self.imagine_tile_layout()
+		self.write_tile_layout()
 
-
-# Also, clean up the above code into routines, objects?
+a = Tile(bg_color="white", fg_color="black", ellipses=[(-1,0,1.1,1), (1,0,1.1,1)], name="tmp", tile_size=[100,100])
+a.create_tile_image()
+a.get_ellipse_edge_colors()
 
 
 # # # 5. Create bot to generate output and post to Instagram
